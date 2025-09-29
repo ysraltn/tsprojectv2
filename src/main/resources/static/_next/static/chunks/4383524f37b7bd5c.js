c8f1c545b9111df187f336231a4b6f88b9625b8a
Yasir <Yasir@MacBook-Air-2.local>
2025-09-29 03:01:12 +0300

    front api conf3

---
+++ src/main/resources/static/_next/static/chunks/4383524f37b7bd5c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/412f69fb44179e1f.js",
+  "static/chunks/turbopack-21e1b376643b3fac.js"
+])
---
+++ src/main/resources/static/_next/static/chunks/cfda75c6470e48ce.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/412f69fb44179e1f.js",
+  "static/chunks/turbopack-359f7a5b3cbadaeb.js"
+])
